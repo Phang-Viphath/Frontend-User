2ca7c30e07a89745ace84241cfde5faecb301aba
viphath12-wq <viphath12@gmail.com>
2026-06-22 23:43:50 +0700

    02

--- src/components/LoginGuest.vue
+++ src/components/LoginGuest.vue
@@ -99,6 +99,6 @@ const submit = async () => {
 }
 
 const handleGoogleLogin = () => {
-  window.location.href = 'http://localhost:8000/auth/guest/google'
+  window.location.href = import.meta.env.GOOGLE
 }
 </script>
